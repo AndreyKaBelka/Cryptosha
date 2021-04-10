2b1f96b3967fbc51561859fe8b0e22accc2b965f
AndreyKaBelka <andreykaov4innikov@yandex.ru>
2021-04-10 20:43:57 +0600

    Initial commit

---
+++ src/main/java/com/messenger/cryptosha/CryptoshaApplication.java
@@ -0,0 +1,13 @@
+package com.messenger.cryptosha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoshaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoshaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/messenger/cryptosha/CryptoshaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.messenger.cryptosha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoshaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
