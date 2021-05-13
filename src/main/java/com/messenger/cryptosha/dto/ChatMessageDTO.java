173d85f554dbcd23904526c2e071abde5040cf83
AndreyKaBelka <andreykaov4innikov@yandex.ru>
2021-05-14 00:22:11 +0600

    add websocket tests

--- src/main/java/com/messenger/cryptosha/dto/ChatMessageDTO.java
+++ src/main/java/com/messenger/cryptosha/dto/ChatMessageDTO.java
@@ -1,10 +1,11 @@
 package com.messenger.cryptosha.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.google.common.base.Objects;
 
 import java.util.Date;
 
-public class ChatMessageDTO {
+public class ChatMessageDTO implements Cloneable {
     private Long id;
     private int senderId;
     private Long chatId;
@@ -53,5 +54,40 @@ public class ChatMessageDTO {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChatMessageDTO that = (ChatMessageDTO) o;
+        return senderId == that.senderId && Objects.equal(id, that.id) && Objects.equal(chatId, that.chatId) && Objects.equal(content, that.content) && Objects.equal(timestamp, that.timestamp);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id, senderId, chatId, content, timestamp);
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        ChatMessageDTO clone = ((ChatMessageDTO) super.clone());
+        clone.setId(this.id);
+        clone.setChatId(this.chatId);
+        clone.setTimestamp(this.timestamp);
+        clone.setContent(this.content);
+        clone.setSenderId(this.senderId);
+        return clone;
+    }
+
+    @Override
+    public String toString() {
+        return "ChatMessageDTO{" +
+                "id=" + id +
+                ", senderId=" + senderId +
+                ", chatId=" + chatId +
+                ", content='" + content + '\'' +
+                ", timestamp=" + timestamp.getTime() +
+                '}';
     }
 }
---
+++ src/test/java/com/messenger/cryptosha/WebSocketStompTest.java
@@ -0,0 +1,87 @@
+package com.messenger.cryptosha;
+
+import com.messenger.cryptosha.dto.ChatMessageDTO;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.simp.stomp.StompFrameHandler;
+import org.springframework.messaging.simp.stomp.StompHeaders;
+import org.springframework.messaging.simp.stomp.StompSession;
+import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+import org.springframework.web.socket.messaging.WebSocketStompClient;
+import org.springframework.web.socket.sockjs.client.SockJsClient;
+import org.springframework.web.socket.sockjs.client.Transport;
+import org.springframework.web.socket.sockjs.client.WebSocketTransport;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class WebSocketStompTest {
+    @Value("${local.server.port}")
+    private int port;
+    private final String URL_SUBSCRIBE = "/user/%d/queue/messages";
+    private final Long userId = 1L;
+    private ChatMessageDTO testMessage;
+
+    private CompletableFuture<ChatMessageDTO> completableFuture;
+    private StompSession stompSession;
+
+    @Before
+    public void setUp() throws Exception {
+        testMessage = new ChatMessageDTO();
+        testMessage.setId(1L);
+        testMessage.setChatId(1L);
+        testMessage.setContent("Test Content Man!");
+        testMessage.setTimestamp(new Date());
+        testMessage.setSenderId(2);
+
+        completableFuture = new CompletableFuture<>();
+        String URL = String.format("http://localhost:%d/ws", port);
+
+        WebSocketStompClient webSocketStompClient = new WebSocketStompClient(new SockJsClient(createTransportClient()));
+        webSocketStompClient.setMessageConverter(new MappingJackson2MessageConverter());
+
+        stompSession = webSocketStompClient.connect(URL, new StompSessionHandlerAdapter() {
+        }).get(1, TimeUnit.SECONDS);
+
+        stompSession.subscribe(String.format(URL_SUBSCRIBE, userId), new MessageStompFrameHandler());
+    }
+
+    @Test
+    public void testMessageGetting() throws ExecutionException, InterruptedException, TimeoutException {
+        stompSession.send(String.format(URL_SUBSCRIBE, userId), testMessage);
+        ChatMessageDTO chatMessageDTO = completableFuture.get(5, TimeUnit.SECONDS);
+
+        Assertions.assertEquals(testMessage, chatMessageDTO);
+    }
+
+    class MessageStompFrameHandler implements StompFrameHandler {
+
+        @Override
+        public Type getPayloadType(StompHeaders stompHeaders) {
+            return ChatMessageDTO.class;
+        }
+
+        @Override
+        public void handleFrame(StompHeaders stompHeaders, Object o) {
+            completableFuture.complete((ChatMessageDTO) o);
+        }
+    }
+
+    private List<Transport> createTransportClient() {
+        List<Transport> transports = new ArrayList<>(1);
+        transports.add(new WebSocketTransport(new StandardWebSocketClient()));
+        return transports;
+    }
+}
