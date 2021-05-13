c3020fa4c2e77d4aadae69d28463719e31274b0e
AndreyKaBelka <andreykaov4innikov@yandex.ru>
2021-05-14 01:07:01 +0600

    fix tests

--- src/test/java/com/messenger/cryptosha/resource/ChatResourceTest.java
+++ src/test/java/com/messenger/cryptosha/resource/ChatResourceTest.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -30,6 +31,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @TestPropertySource(
         locations = "classpath:application-test.properties"
 )
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 @WebMvcTest(ChatResource.class)
 class ChatResourceTest {
     @Autowired
