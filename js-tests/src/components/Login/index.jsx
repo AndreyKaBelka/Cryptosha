724906541d8ba1c9003a0e8242892ad929761b0a
AndreyKaBelka <andreykaov4innikov@yandex.ru>
2021-06-13 00:47:11 +0600

    add registration and cookie

--- js-tests/src/components/Login/index.jsx
+++ js-tests/src/components/Login/index.jsx
@@ -1,11 +1,21 @@
 import React, {Component} from "react";
 
-
 export default class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.onLoginBtnClick = this.onLoginBtnClick.bind(this);
+    }
+
+    onLoginBtnClick(event) {
+        const username = $("#username").val();
+        console.log(username);
+    }
+
     render() {
         return (
             <div>
-                <h3>LOGIN!</h3>
+                <input type='text' id='username'/>
+                <button onClick={this.onLoginBtnClick} id='Login'>Login</button>
             </div>
         )
     }
---
+++ js-tests/src/components/Registration/Registered/index.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+export default class Registered extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Congrats, you are successfully registered!</h3>
+            </div>
+        )
+    }
+}
